3b8cc4f767980da7df70b6d35057991d02435b6d
jcgoodwin86 <4566470+jcgoodwin86@users.noreply.github.com>
2025-10-06 11:56:52 -0400

    Add config package to manage user config file

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/jcgoodwin86/blogagg/internal/config"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	cfg, err := config.Read()
+	if err != nil {
+		log.Fatalf("error reading config: %v", err)
+	}
+	fmt.Printf("Read config: %+v\n", cfg)
+
+	err = cfg.SetUser("lane")
+	if err != nil {
+		log.Fatalf("couldn't set current user: %v", err)
+	}
+
+	cfg, err = config.Read()
+	if err != nil {
+		log.Fatalf("error reading config: %v", err)
+	}
+	fmt.Printf("Read config again: %+v\n", cfg)
 }
